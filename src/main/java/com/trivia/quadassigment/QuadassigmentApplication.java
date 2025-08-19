b1686610a90bf7bf0230ef4b7ab535043a25e3be
FeRiiD23 <f.ilica@ifsoftwaresolutions.nl>
2025-08-19 20:36:37 +0200

    added project
    added created project from spring initializer

---
+++ src/main/java/com/trivia/quadassigment/QuadassigmentApplication.java
@@ -0,0 +1,13 @@
+package com.trivia.quadassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadassigmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadassigmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trivia/quadassigment/QuadassigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivia.quadassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadassigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
